61d8405bd16ba5d98fadfc4ae890603e6989cf45
igor1251 <84015785+igor1251@users.noreply.github.com>
2022-03-23 21:38:24 +0300

    Update fetch-users.js

--- webpanel/js/fetch-users.js
+++ webpanel/js/fetch-users.js
@@ -1,9 +1,11 @@
-// fetch('https://jsonplaceholder.typicode.com/posts/1')
-//   .then((response) => response.json())
-//   .then((json) => console.log(json));
-for (let i = 0; i < 10; i++)
-{
-    let row = document.createElement('tr');
-    row.innerHTML = "<td>Иванов Анатолий Викторович</td><td>89610037151</td><td>Тест</td>";
-    document.getElementById("usersTable").appendChild(row);
+function displayItems(data) {
+    data.forEach(user => {
+        let row = document.createElement('tr');
+        row.innerHTML = `<td>${user.userName}</td><td>${user.userPhone}</td><td>${user.userComment}</td>`;
+        document.getElementById("usersTable").appendChild(row);
+    });
 }
+
+fetch('http://localhost:59562/api/users/db')
+    .then(response => response.json())
+    .then(data => displayItems(data));
