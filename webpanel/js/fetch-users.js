1e5ccbc7055d42387f6130467fe27c77152086f9
igor1251 <84015785+igor1251@users.noreply.github.com>
2022-03-23 15:49:21 +0300

    Добавил кучу вкусностей

--- webpanel/js/fetch-users.js
+++ webpanel/js/fetch-users.js
@@ -1,3 +1,9 @@
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+// fetch('https://jsonplaceholder.typicode.com/posts/1')
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+for (let i = 0; i < 10; i++)
+{
+    let row = document.createElement('tr');
+    row.innerHTML = "<td>Иванов Анатолий Викторович</td><td>89610037151</td><td>Тест</td>";
+    document.getElementById("usersTable").appendChild(row);
+}
