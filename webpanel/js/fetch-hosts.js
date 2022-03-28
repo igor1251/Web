1d0bca11ec2e94df9c2fdf7b53496f393d381729
igor1251 <84015785+igor1251@users.noreply.github.com>
2022-03-28 13:38:32 +0300

    немного косметики и логики

--- webpanel/js/fetch-hosts.js
+++ webpanel/js/fetch-hosts.js
@@ -4,6 +4,20 @@
 for (let i = 0; i < 5; i++)
 {
     let row = document.createElement('tr');
-    row.innerHTML = "<td>192.168.34.56</td><td>DESKTOP-0SER98T</td><td>443</td>";
+    row.innerHTML = `
+        <td>
+            192.168.34.56
+        </td>
+        <td>
+            DESKTOP-0SER98T
+        </td>
+        <td>
+            443
+        </td>
+        <td>
+            <form>
+                <input type="submit" action="http://localhost:59562/api/hosts/${i}" method="DELETE" value="удалить"></input>
+            </form>
+        </td>`;
     document.getElementById("usersTable").appendChild(row);
 }
--- webpanel/js/fetch-users.js
+++ webpanel/js/fetch-users.js
@@ -1,8 +1,15 @@
-const url = 'http://localhost:5000/api/users/db';
+const url = 'http://localhost:59562/api/users/';
+
+function sendDeleteRequest(userID) {
+    fetch(url + userID, {
+        method: "DELETE",
+    });
+}
 
 function displayItems(data) {
     data.forEach(user => {
         let row = document.createElement('tr');
+        row.id = `${user.userID}`;
         row.innerHTML = `
             <td>
                 ${user.userName}
@@ -15,13 +22,13 @@ function displayItems(data) {
             </td>
             <td style="text-align: right;">
                 <button class="btn-action btn-search" onclick="localStorage.setItem('userID', ${user.id}); window.location.href='current-user.html'"></button>
-                <button class="btn-action btn-delete" onclick=""></button>
+                <button class="btn-action btn-delete" onclick="sendDeleteRequest(${user.id});"></button>
             </td>`;
         document.getElementById("usersTable").appendChild(row);
     });
 }
 
-fetch(url)
+fetch(url + "db")
     .then(response => response.json())
     .then(users => displayItems(users))
     .catch(exception => console.log(exception));
